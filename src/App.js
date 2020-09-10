9dcca6f5cd5e8b87b5c177f0cbdc8a7f9d8d3a70
bexking <bex.king@icloud.com>
2020-09-10 20:02:15 +1200

    deleting unnecessary stuff that came with create react app and adding basic setup for to do list app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="title">
+        <h1>Reminders:</h1>
+      </div>
+        <ul className="list">
+            <textarea>Item One</textarea>
+            <textarea>Item Two</textarea>
+            <textarea>Item Three</textarea>
+            <textarea>Item Four</textarea>
+        </ul>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Pages/JS/ToDo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ToDo() {
+    return (
+        <div>
+            <h1>Reminders:</h1>
+            <ul>
+                <textarea>Item One</textarea>
+                <textarea>Item Two</textarea>
+                <textarea>Item Three</textarea>
+                <textarea>Item Four</textarea>
+            </ul>
+        </div>
+    )
+}
